b92b98134d29d38c0d5baa20f4a41798ea373426
mengpingzhu51-lang <mengpingzhu51@gmail.com>
2026-06-03 15:25:09 +0800

    Add Vercel deployment config

--- server.ts
+++ server.ts
@@ -444,6 +444,10 @@ async function startServer() {
   });
 }
 
-startServer().catch((err) => {
-  console.error("Failed to start server:", err);
-});
+export default app;
+
+if (!process.env.VERCEL) {
+  startServer().catch((err) => {
+    console.error("Failed to start server:", err);
+  });
+}
